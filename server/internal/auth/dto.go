66e381911eb22d767ed24c1ddc2eac9c76bc3399
Aprim <aprimregmi24@gmail.com>
2026-09-18 15:02:00 +0545

    feat(auth): add validate methods to validate the request body

--- server/internal/auth/dto.go
+++ server/internal/auth/dto.go
@@ -1,5 +1,7 @@
 package auth
 
+import "github.com/aprimr/tickr/internal/pkg/validate"
+
 type LoginRequest struct {
 	Email    string `json:"email" validate:"required"`
 	Password string `json:"password" validate:"required"`
@@ -21,4 +23,56 @@ type VenueRegisterRequest struct {
 	Address      string `json:"address" validate:"required"`
 	City         string `json:"city" validate:"required"`
 	TotalScreens int    `json:"total_screens" validate:"required, min=1"`
+}
+
+// Validate UserRegisterRequest data
+func (req *UserRegisterRequest) Validate() map[string]string {
+	errs := make(map[string]string)
+
+	if !validate.IsMinLength(req.FullName, 4) {
+		errs["fullname"] = "full name must be greater than 4 characters"
+	}
+	if !validate.IsEmail(req.Email) {
+		errs["email"] = "invalid email format"
+	}
+	if !validate.IsPhone(req.PhoneNumber) {
+		errs["phone_number"] = "invalid phone number format"
+	}
+	if !validate.IsValidPassword(req.Password) {
+		errs["password"] = "password must be at least 8 characters long and contain at least one number and one special character (! @ # $ % ^ & * ? )"
+	}
+
+	return errs
+}
+
+// Validate VenueRegisterRequest data
+func (req *VenueRegisterRequest) Validate() map[string]string {
+	errs := make(map[string]string)
+
+	if !validate.IsEmail(req.Email) {
+		errs["email"] = "invalid email format"
+	}
+	if !validate.IsPhone(req.PhoneNumber) {
+		errs["phone_number"] = "invalid phone number format"
+	}
+	if !validate.IsValidPassword(req.Password) {
+		errs["password"] = "password must be at least 8 characters long and contain at least one number and one special character (! @ # $ % ^ & * ? )"
+	}
+	if !validate.IsNotBlank(req.VenueName) {
+		errs["venue_name"] = "venue name is required"
+	}
+	if !validate.IsMinLength(req.VenueName, 5) {
+		errs["venue_name"] = "venue name must be atleast 5 characters long"
+	}
+	if !validate.IsNotBlank(req.Address) {
+		errs["address"] = "address is required"
+	}
+	if !validate.IsNotBlank(req.City) {
+		errs["city"] = "city is required"
+	}
+	if req.TotalScreens < 1 {
+		errs["total_screens"] = "total screens must be at least 1"
+	}
+
+	return errs
 }
