c3d27e07c295b343a90ae570afd97d838990af3e
Aprim <aprimregmi24@gmail.com>
2026-09-22 15:39:21 +0545

    fix(migration): typo in table name

--- server/db/migrations/20260918035929_init_users_schema.sql
+++ server/db/migrations/20260918035929_init_users_schema.sql
@@ -60,7 +60,7 @@ CREATE TABLE admin_details (
 DROP TABLE IF EXISTS admin_details;
 DROP TABLE IF EXISTS venue_details;
 DROP TABLE IF EXISTS user_details;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS users;
 
 -- Enums
 DROP TYPE IF EXISTS venue_status;
