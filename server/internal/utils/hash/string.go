26fb6510b8151af522ede75cd91514b2dba212c6
Aprim <aprimregmi24@gmail.com>
2026-09-24 15:14:50 +0545

    refactor(hash): switch String and CheckString to SHA-256 and add bcrypt hash utils

--- server/internal/utils/hash/string.go
+++ server/internal/utils/hash/string.go
@@ -1,12 +1,32 @@
 package hash
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
-// String takes a plain text string and returns its bcrypt hash
+// String takes a plain text string and returns its SHA-256 hash
+// It will always return the exact same output, for a given string
 func String(input string) (string, error) {
+	hashBytes := sha256.Sum256([]byte(input))
 
+	return hex.EncodeToString(hashBytes[:]), nil
+}
+
+// CheckString compares a plain text string with a hashed string
+func CheckString(input, hash string) bool {
+	incomingHash, err := String(input)
+	if err != nil {
+		return false
+	}
+
+	return incomingHash == hash
+}
+
+// BcryptString takes a short plain string and returns its bcrypt hash.
+func BcryptString(input string) (string, error) {
 	if len(input) > 72 {
 		return "", ErrStringTooLong
 	}
@@ -19,8 +39,9 @@ func String(input string) (string, error) {
 	return string(bytes), nil
 }
 
-// CheckString compares a plain text string with a hashed string
-func CheckString(input, hash string) bool {
+// CheckBcryptString compares a plaintext string with a bcrypt hash.
+func CheckBcryptString(input, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(input))
+
 	return err == nil
 }
